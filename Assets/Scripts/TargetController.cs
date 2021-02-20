35bd12607bfa2b87c68b9508ae5daf509ec81e4c
Chris Greenway <slothropbodine@gmail.com>
2021-02-20 18:26:10 -0500

    Prefabs for patterns + tweaks

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -9,6 +9,8 @@ public class TargetController : MonoBehaviour
     public int hitPoints = 100;
     public float speed = 1.0f;
     public float failMeterContribution;
+    public float failMeterHitSubtractionFactor;
+    public Vector3 direction;
 
     public float FailMeterContribution {
         get => this.failMeterContribution;
@@ -16,8 +18,10 @@ public class TargetController : MonoBehaviour
     }
 
     void Update() {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
-        GameManager.instance.IncreaseMeter(this.FailMeterContribution * Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime);
+        if (Time.time > 5) {
+            GameManager.instance.IncreaseMeter(this.FailMeterContribution * Time.deltaTime);
+        }
     }
     
     void OnTriggerEnter2D(Collider2D other) {
@@ -25,14 +29,13 @@ public class TargetController : MonoBehaviour
             hits += 1;
             GameManager.instance.IncreaseScore(pointValue);
             Destroy(other.gameObject);
-            GameManager.instance.IncreaseMeter(-this.FailMeterContribution * 7.5f * Time.deltaTime);
+            GameManager.instance.IncreaseMeter(-this.FailMeterContribution * failMeterHitSubtractionFactor * Time.deltaTime);
 
             if (hits == hitPoints) {
-                Debug.Log("gay");
                 Destroy(this.gameObject);
             }
 
-            this.transform.localScale = new Vector3(.5f, Mathf.Lerp(5, 1, (float)hits / hitPoints), 1);
+            this.transform.localScale = new Vector3(.5f, Mathf.Lerp(5, .6f, (float)hits / hitPoints), 1);
         }
         else {
             speed = speed * -1.0f;
